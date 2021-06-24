eeea0b8348db77dc72cf16180288ecabff90f002
saitejareddy007 <tejareddy.d321@gmail.com>
2021-06-24 15:50:58 +0530

    adding apis to perform CRUD operations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const {callAPI, fn} = require('../lib/common-utils/router_functions');
+const InventoryAPIs = require('../lib/apis/inventoryAPIs').getInstance();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/createProduct', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'createProduct'));
+});
+
+router.post('/getProductData', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'getProductData'));
+});
+
+router.post('/addInventoryToWarehouse', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'addInventoryToWarehouse'));
+});
+
+router.post('/pushInventoryToReservedOrDamaged', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'pushInventoryToReservedOrDamaged'));
+});
+
+router.post('/getMasterInventoryViewWithSkuId', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'getMasterInventoryViewWithSkuId'));
+});
+
+router.post('/getProductsWithFilter', async (req, res) => {
+  callAPI(req, res, fn.bind(InventoryAPIs, 'getProductsWithFilter'));
 });
 
 module.exports = router;
