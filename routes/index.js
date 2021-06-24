3d7317d3610192745cd09d398c173e84ba201599
saitejareddy007 <tejareddy.d321@gmail.com>
2021-06-24 17:01:30 +0530

    adding config for db connections

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var router = express.Router();
-const {callAPI, fn} = require('../lib/common-utils/router_functions');
+const {callAPI, bind} = require('../lib/common-utils/router_functions');
 const InventoryAPIs = require('../lib/apis/inventoryAPIs').getInstance();
 
 /* GET home page. */
 router.post('/createProduct', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'createProduct'));
+  callAPI(req, res, bind(InventoryAPIs, 'createProduct'));
 });
 
 router.post('/getProductData', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'getProductData'));
+  callAPI(req, res, bind(InventoryAPIs, 'getProductData'));
 });
 
 router.post('/addInventoryToWarehouse', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'addInventoryToWarehouse'));
+  callAPI(req, res, bind(InventoryAPIs, 'addInventoryToWarehouse'));
 });
 
 router.post('/pushInventoryToReservedOrDamaged', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'pushInventoryToReservedOrDamaged'));
+  callAPI(req, res, bind(InventoryAPIs, 'pushInventoryToReservedOrDamaged'));
 });
 
 router.post('/getMasterInventoryViewWithSkuId', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'getMasterInventoryViewWithSkuId'));
+  callAPI(req, res, bind(InventoryAPIs, 'getMasterInventoryViewWithSkuId'));
 });
 
 router.post('/getProductsWithFilter', async (req, res) => {
-  callAPI(req, res, fn.bind(InventoryAPIs, 'getProductsWithFilter'));
+  callAPI(req, res, bind(InventoryAPIs, 'getProductsWithFilter'));
 });
 
 module.exports = router;
